a52f4789ba9748a27021babbe03cb0b7df8cced2
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2024-12-10 22:03:23 +0900

    マイグレーションファイル作成

---
+++ src/database/migrations/2024_12_08_151924_create_animations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('animations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->bigInteger('term_id')->nullable();
+
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('animations');
+    }
+};
