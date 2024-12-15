fa8c8ff8e202e67ef5c2f54e506b960f72a1e683
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2024-12-15 18:08:10 +0900

    デザインの修正

--- src/resources/js/Pages/Web/User/Term/ListView/index.tsx
+++ src/resources/js/Pages/Web/User/Term/ListView/index.tsx
@@ -1,62 +1,68 @@
 
-import { AnimeCard } from '@/Components/AnimeCard'
 import { getSeasonText } from '@/hooks/anime'
-import { Anime } from '@/types/anime'
+import { useTerm } from '@/hooks/useTerm'
 import { router } from '@inertiajs/react'
 import React, { useCallback, useState } from 'react'
 
+
+type Term = {
+    id: string,
+    year: string,
+    season: string
+}
 type Props = {
-    user?: any,
-    terms: {
-        id: string,
-        year: string,
-        season: string
-    }[]
+    terms: Term[]
 }
 
 export const ListView = React.memo<Props>(function ListView({
-    user,
     terms
 }) {
+    const { groupedTerms } = useTerm()
+    const grouped = groupedTerms(terms)
 
-    const [activeId] = useState<string>()
-
-    const handleClick = (termId: string) => {
-        router.visit(route("web.user.term.show", { user_id: user?.id, term_id: termId }))
+    const handleClick = (id: string) => {
+        router.visit(route("web.term.show", id))
     }
+
     return (
-        <>
+        <div className='p-4'>
             <p className='text-gray-700p-4 p-4 text-2xl font-bold'>
                 アニメシーズン一覧
             </p>
+            {Object.keys(grouped).map((year) => (
+                <div key={year} className="mb-6">
+                    {/* Year Header */}
+                    <h2 className="mb-4 text-lg font-semibold text-gray-900 sm:text-xl">
+                        {year} 年
+                    </h2>
 
-            <div className="grid grid-cols-2 gap-4 p-4 sm:grid-cols-4 md:grid-cols-4">
-                {terms.map((term) => (
-                    <button
-                        key={term.id}
-                        onClick={() => handleClick(term.id)}
-                        className={`
-                            flex h-16 w-full
-                            transform items-center
-                            justify-center
-                            rounded-lg
-                            shadow-md transition-transform
-                            hover:scale-105
-                            focus:outline-none
-                            focus:ring-4 focus:ring-blue-300
-                            ${activeId === term.id
-                                ? "bg-blue-600 text-white shadow-lg"
-                                : "bg-white text-gray-800 hover:bg-blue-100"
-                            }
+                    {/* Seasons Buttons */}
+                    <div className="grid grid-cols-4 gap-4">
+                        {grouped[year].map((term) => (
+                            <button
+                                key={term.id}
+                                onClick={() => handleClick(term.id)}
+                                className={`
+                                flex h-16 w-full
+                                transform items-center
+                                justify-center
+                                rounded-lg
+                                bg-white text-gray-800
+                                shadow-md
+                                transition-transform
+                                hover:scale-105 hover:bg-blue-100
+                                focus:outline-none focus:ring-4 focus:ring-blue-300
                             `}
-                    >
-                        <span className="text-sm font-medium sm:text-base">
-                            {term.year} 年 {getSeasonText(term.season)}
-                        </span>
-                    </button>
-                ))}
-            </div>
-        </>
+                            >
+                                <span className="text-sm font-medium sm:text-base">
+                                    {getSeasonText(term.season)}
+                                </span>
+                            </button>
+                        ))}
+                    </div>
+                </div>
+            ))}
+        </div >
     )
 
 })
