6a9399776e0ec48837f9cc4b3d2a984beb325861
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2024-12-17 21:43:46 +0900

    修正

--- src/resources/js/Pages/Web/User/AnimationRank/ShowView/index.stories.tsx
+++ src/resources/js/Pages/Web/User/AnimationRank/ShowView/index.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react'
-import EditView from '.'
+import ShowView from '.'
 
 const meta: Meta<typeof ShowView> = {
     title: 'views/Web/User/AnimationRank/ShowView',
