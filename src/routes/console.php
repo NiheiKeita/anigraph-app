355b4b177d44d122c847e933045b8b90927fad84
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2025-01-14 20:09:01 +0900

    毎週月曜の２４時に流れるように修正

--- src/routes/console.php
+++ src/routes/console.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Schedule;
 use Illuminate\Support\Facades\Artisan;
 
 /*
@@ -17,3 +18,4 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+Schedule::command('app:get-term-animations')->cron('0 0 * * 1');
