14dafdb71261d7e76a71c9699fc27e9b96ebdd5f
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2025-01-15 20:53:21 +0900

    metaタグ修正

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="title" content="{{ config('app.name', 'あにぐらふ') }}">
 
         <title inertia>{{ config('app.name', 'あにぐらふ') }}</title>
 
