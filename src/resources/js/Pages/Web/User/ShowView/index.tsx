d17818a2e70289e2c88024459bca57ce77d8bab8
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2025-04-13 07:59:56 +0900

    fix not tv bug

--- src/resources/js/Pages/Web/User/ShowView/index.tsx
+++ src/resources/js/Pages/Web/User/ShowView/index.tsx
@@ -22,7 +22,13 @@ export const ShowView = React.memo<Props>(function ShowView({
                 <Button className='ml-4 w-fit' onClick={() => router.visit(route('web.user.term.list', user?.id))}>
                     {user?.name}が見たアニメを見る(シーズン一覧)
                 </Button>
-                <Button className='ml-4 w-fit' onClick={() => router.visit(route('web.user.animations.show.evaluation', user?.id))}>
+                <Button className='ml-4 w-fit' onClick={() => {
+                    router.visit(route("web.user.animations.show.evaluation", user?.id), {
+                        data: {
+                            media: "tv",
+                        },
+                    })
+                }}>
                     {user?.name}が評価したアニメを見る
                 </Button>
             </div>
@@ -31,7 +37,13 @@ export const ShowView = React.memo<Props>(function ShowView({
                     <Button className='ml-4 w-fit' onClick={() => router.visit(route('web.user.term.edit.list.viewingStatus', user?.id))}>
                         視聴アニメを編集する
                     </Button>
-                    <Button className='ml-4 w-fit' onClick={() => router.visit(route('web.user.animations.edit.evaluation', user?.id))}>
+                    <Button className='ml-4 w-fit' onClick={() => {
+                        router.visit(route("web.user.animations.edit.evaluation", user?.id), {
+                            data: {
+                                media: "tv",
+                            },
+                        })
+                    }}>
                         アニメの評価を編集する
                     </Button>
                 </div>
